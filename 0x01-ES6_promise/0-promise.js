991479643dafabb6c60ace024aac69c7b715d720
ysfzxz7 <bbbmaroc83@gmail.com>
2024-05-28 18:25:38 +0100

    file0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
 export default function getResponseFromAPI() {
-  const pro = new Promise((resolve) => resolve('Task resolved'));
+  const pro = new Promise((resolve) => resolve("Task resolved"));
   return pro;
 }
