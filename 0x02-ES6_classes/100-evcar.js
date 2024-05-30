fc149b6af4477300a302362c3a6e0f1aaa8724b2
ysfzxz <bbbmaroc@gmail.com>
2024-05-30 21:10:55 +0100

    time for rest

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,7 +1,7 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, color, motor, range) {
+  constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
