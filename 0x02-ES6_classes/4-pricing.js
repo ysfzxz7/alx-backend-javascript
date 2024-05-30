7d56af95d3480f863c289ef3e5dfeb0ab5042938
ysfzxz <bbbmaroc@gmail.com>
2024-05-30 15:50:17 +0100

    what the heck  is going on ?

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount(amount) {
+    if ((typeof amount !== 'number') && !(amount instanceof Number)) throw new Error('Number must be a number');
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(currency) {
+    if ((currency instanceof Currency)) this._currency = currency;
+    else throw new TypeError('Currency must be a currency');
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if ((typeof amount !== 'number') && !(amount instanceof Number)) throw new TypeError('Amount must be a number');
+    if ((typeof conversionRate !== 'number') && !(conversionRate instanceof Number)) throw new TypeError('conversionRate must be a number');
+    return amount * conversionRate;
+  }
+}
