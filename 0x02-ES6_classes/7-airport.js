9e1bde7fc6d19db386fd5df23a4f991a888b16c6
ysfzxz <bbbmaroc@gmail.com>
2024-05-30 16:43:06 +0100

    hidden discription

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,28 @@
+export default class {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._code;
+  }
+
+  set name(name) {
+    if ((typeof name !== 'string') && !(name instanceof String)) throw new TypeError('name must be a string');
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if ((typeof code !== 'string') && !(code instanceof String)) throw new TypeError('code must be a string');
+    this._code = code;
+  }
+
+  toString() {
+    return `[Object ${this._code}]`;
+  }
+}
