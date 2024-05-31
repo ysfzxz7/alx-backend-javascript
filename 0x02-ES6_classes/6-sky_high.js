932bf3444b2f521eb0814c7acc070b309fd0e250
ysfzxz <bbbmaroc@gmail.com>
2024-05-31 10:55:08 +0100

    edit the file6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,11 +3,6 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
-  }
-
-  set floors(floors) {
-    if ((typeof floors !== 'number') throw new TypeError('Floors must be a number');
     this._floors = floors;
   }
 
@@ -15,7 +10,12 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
+  set floors(floors) {
+    if (typeof floors !== 'number') throw new TypeError('Floors must be a number');
+    this._floors = floors;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floor} floors`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
