cb7eef774660fa5b3e8b8010ab0cdf16c5a2ea39
ysfzxz <bbbmaroc@gmail.com>
2024-06-03 19:47:56 +0100

    100-weakmap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,11 @@
 export const weakMap = new Map();
 
+const MAX_ENDPOINT_CALLS = 5;
+
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) throw new Error('Endpoint load is high');
 }
