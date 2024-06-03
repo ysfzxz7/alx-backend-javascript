567c61200128ca40180bca7a6e8210ccc65b0790
ysfzxz <bbbmaroc@gmail.com>
2024-06-03 19:09:34 +0100

    files 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, s) {
   if (!s || s.length === 0 || !set || typeof s !== 'string' || !(set instanceof Set)) return '';
   const arr = [];
-  for (const i of set) {
-    if (i.startsWith(s)) arr.push(i.slice(s.length));
+  for (const i of set.values()) {
+    if (typeof i === 'string' && i.startsWith(s)) arr.push(i.slice(s.length));
   }
   return arr.join('-');
 }
