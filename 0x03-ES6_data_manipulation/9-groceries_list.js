4479338a878a1861bee8b15582d5f94ebcf1bb52
ysfzxz <bbbmaroc@gmail.com>
2024-06-03 19:18:49 +0100

    init new map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,5 @@
+export default function groceriesList() {
+  const data = [['Apples', 10], ['Tomatoes', 1], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
+  const map = new Map(data);
+  return map;
+}
