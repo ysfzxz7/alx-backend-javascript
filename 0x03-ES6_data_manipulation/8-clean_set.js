a54ab5934cb321793c9b895d8b82e163f0edba85
ysfzxz <bbbmaroc@gmail.com>
2024-06-03 19:04:09 +0100

    files 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,5 @@
 export default function cleanSet(set, s) {
-  if (!s) return;
-  if ()
+  if (!s || s.length === 0 || !set || !(typeof s !== 'string' || !(set instanceof Set))) return '';
   const arr = [];
   for (const i of set) {
     if (i.startsWith(s)) arr.push(i.slice(s.length));
